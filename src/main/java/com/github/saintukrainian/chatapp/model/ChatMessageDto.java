f01bc625cfd47dc57f400c9a80d98d4cc871eb7e
Denys Matsenko <Denys.Matsenko@davita.com>
2022-09-04 19:27:07 +0300

    Update and Delete functionality

--- src/main/java/com/github/saintukrainian/chatapp/model/ChatMessageDto.java
+++ src/main/java/com/github/saintukrainian/chatapp/model/ChatMessageDto.java
@@ -1,5 +1,6 @@
 package com.github.saintukrainian.chatapp.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import com.github.saintukrainian.chatapp.jackson.serializer.LocalDateTimeSerializer;
 import java.time.LocalDateTime;
@@ -15,8 +16,16 @@ import lombok.NoArgsConstructor;
 public class ChatMessageDto {
 
   private Long chatId;
+
+  private Long messageId;
+
   @JsonSerialize(using = LocalDateTimeSerializer.class)
   private LocalDateTime sendTimestamp;
+
   private String value;
+
   private UserDto fromUser;
+
+  @JsonProperty("isEdited")
+  private boolean edited;
 }
